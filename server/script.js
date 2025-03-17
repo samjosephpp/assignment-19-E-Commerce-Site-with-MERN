7f887597322e7f82043060086946b64c1a0da2bc
Sam <samjoseph_p@hotmail.com>
2025-03-18 00:22:20 +0300

    Update Server. Added allowHeaders

--- server/script.js
+++ server/script.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: 'https://assignment-19-e-commerce-site-with-mern.vercel.app', //'http://localhost:5173',
-    credentials: true
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token']     // added to remove error while vercel deployment
 }));
 
 
